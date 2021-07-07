e7f21e38063fd7acf9d06dfdaaea6c72db3a12d1
igor <igorsife80@gmail.com>
2021-07-07 14:59:29 -0300

    cumpre o requisito 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { title, storyline, imagePath, id } = this.props.movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={ imagePath } alt={ title } />
+        <h2>{title}</h2>
+        <p>{storyline}</p>
+        <Link to={ `movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+
 import MovieCard from '../components/MovieCard';
 import { Loading } from '../components';
 
