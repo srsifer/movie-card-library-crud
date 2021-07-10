57bb4a86c37fa46c5ab63c9b06535de58c006991
igor <igorsife80@gmail.com>
2021-07-10 10:31:18 -0300

    completa requisito 6 corrigindo o endereço da rota

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import MovieCard from '../components/MovieCard';
 import { Loading } from '../components';
@@ -41,6 +42,9 @@ class MovieList extends Component {
           )
           : (
             <section>
+              <div>
+                <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+              </div>
               {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
             </section>
           )}
